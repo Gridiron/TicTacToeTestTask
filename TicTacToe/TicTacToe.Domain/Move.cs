13312198afbd7a0a9e90a6244792e9a0c361da96
Mikita Ishchanka <m.ishchanka@godeltech.com>
2023-09-05 13:34:51 +0200

    Move added

---
+++ TicTacToe/TicTacToe.Domain/Move.cs
@@ -0,0 +1,16 @@
+﻿namespace TicTacToe.Domain
+{
+    public class Move
+    {
+        public Coordinate Coordinate { get; }
+        public char PlayerSymbol { get; }
+        public int Row => Coordinate.Row;
+        public int Column => Coordinate.Column;
+
+        public Move(Coordinate coordinate, char playerSymbol)
+        {
+            Coordinate = coordinate;
+            PlayerSymbol = playerSymbol;
+        }
+    }
+}
